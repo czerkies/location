2bbdccc14080307d617d431cb2183f7f5a683667
roman <roman.czerkies@gmail.com>
2015-12-12 15:39:38 +0100

    Fix(gestions-produits): Factorisation code
    Factorisation code
    
    Issue #15

--- controleurs/controleursProduitAdmin.php
+++ controleurs/controleursProduitAdmin.php
@@ -68,15 +68,56 @@ class controleursProduitAdmin extends controleursSuper {
     $ajouter = FALSE;
 
     $date = new controleursFonctions();
-
     $salles = new modelesSalles();
-    $affichageSalles = $salles->affichageSalles();
-
     $listePromo = new modelesPromotion();
-    $affichagePromo = $listePromo->affichageCodePromo();
-
     $pdo = new modelesProduit();
-    $affichageProduitsAdmin = $pdo->affichageProduitsAdmin();
+
+    // Supprésion d'une salle
+    if(isset($_GET['supp']) && !empty($_GET['supp'])){
+
+      $id_produit_supp = htmlentities($_GET['supp']);
+      $suppressionProduitAdmin = $pdo->suppressionProduitAdmin($id_produit_supp);
+
+    }
+
+    // Modification d'un produit Admin
+    if(isset($_GET['modif'])) {
+
+      $id_produit_modif = $_GET['modif'];
+      $idProduitModif = $pdo->recupProduitParID($id_produit_modif);
+
+      if(isset($_POST) && !empty($_POST)){
+
+        if(empty($_POST['prix'])){
+          $msg .= "Veuillez saisir un prix.<br>";
+        }
+
+        if(empty($msg)){
+
+          foreach ($_POST as $key => $value){
+            $_POST[$key] = htmlentities($value, ENT_QUOTES);
+          }
+
+          extract($_POST);
+
+          $date_arrivee = $_POST['date_arrivee_A'].'-'.$_POST['date_arrivee_M'].'-'.$_POST['date_arrivee_J'];
+          $date_arrivee .= ' '.$_POST['date_arrivee_H'].':'.$_POST['date_arrivee_I'];
+
+          $date_depart = $_POST['date_depart_A'].'-'.$_POST['date_depart_M'].'-'.$_POST['date_depart_J'];
+          $date_depart .= ' '.$_POST['date_depart_H'].':'.$_POST['date_depart_I'];
+
+          $id_promo = (isset($_POST['id_promo']) && $_POST['id_promo'] === 'NULL') ? NULL : $_POST['id_promo'];
+
+          $modifProduit = new modelesProduit();
+          $modifProduit->updateProduitAdmin($id_salle, $date_arrivee, $date_depart, $prix, $id_promo, $id_produit);
+
+          $msg .= 'Le produit a bien été modifié.';
+
+        }
+
+      }
+
+    }
 
     // ORDER BY pour desc de date_arrivee, date_depart et prix.
     if(isset($_GET['type']) && isset($_GET['order'])){
@@ -87,63 +128,22 @@ class controleursProduitAdmin extends controleursSuper {
 
         $type = htmlentities($_GET['type']);
         $order = htmlentities($_GET['order']);
-        $affichageProduitsAdmin = $pdo->affichageProduitsAdminTypeOrder($_GET['type'], $_GET['order']);
 
-      } else {
+        $affichageProduitsAdmin = $pdo->affichageProduitsAdminTypeOrder($type, $order);
 
-          $affichageProduitsAdmin = $pdo->affichageProduitsAdmin();
       }
 
-    } elseif(isset($_GET['supp'])) {
-      $id_produit_supp = htmlentities($_GET['supp']);
-      $suppressionProduitAdmin = $pdo->suppressionProduitAdmin($id_produit_supp);
-      $affichageProduitsAdmin = $pdo->affichageProduitsAdmin();
     } else {
 
-      // Modification d'un produit Admin
       $affichageProduitsAdmin = $pdo->affichageProduitsAdmin();
 
-      if(isset($_GET['modif'])) {
-
-        $id_produit_modif = $_GET['modif'];
-        $idProduitModif = $pdo->recupProduitParID($id_produit_modif);
-
-        if(isset($_POST) && !empty($_POST)){
-
-          if(empty($_POST['prix'])){
-            $msg .= "Veuillez saisir un prix.<br>";
-          }
-
-          if(empty($msg)){
-
-            foreach ($_POST as $key => $value){
-              $_POST[$key] = htmlentities($value, ENT_QUOTES);
-            }
-
-            extract($_POST);
-
-            $date_arrivee = $_POST['date_arrivee_A'].'-'.$_POST['date_arrivee_M'].'-'.$_POST['date_arrivee_J'];
-            $date_arrivee .= ' '.$_POST['date_arrivee_H'].':'.$_POST['date_arrivee_I'];
-
-            $date_depart = $_POST['date_depart_A'].'-'.$_POST['date_depart_M'].'-'.$_POST['date_depart_J'];
-            $date_depart .= ' '.$_POST['date_depart_H'].':'.$_POST['date_depart_I'];
-
-            $id_promo = (isset($_POST['id_promo']) && $_POST['id_promo'] === 'NULL') ? NULL : $_POST['id_promo'];
-
-            $modifProduit = new modelesProduit();
-            $modifProduit->updateProduitAdmin($id_salle, $date_arrivee, $date_depart, $prix, $id_promo, $id_produit);
-
-            $msg .= 'Le produit a bien été modifié.';
-
-            $affichageProduitsAdmin = $pdo->affichageProduitsAdmin();
-
-          }
-
-        // Appel de la requete d'insertion si $msg est vide.
-        }
-
-      }
     }
+
+    $affichageSalles = $salles->affichageSalles();
+    $affichagePromo = $listePromo->affichageCodePromo();
+
+
+
 
     $this->Render('../vues/produit/gestion_produits.php', array('msg' => $msg, 'userConnect' => $userConnect, 'userConnectAdmin' => $userConnectAdmin, 'date' => $date, 'ajouter' => $ajouter, 'affichageProduitsAdmin' => $affichageProduitsAdmin, 'affichageSalles' => $affichageSalles, 'affichagePromo' => $affichagePromo, 'idProduitModif' => $idProduitModif));
 
