bd6fb1d63a3f5706158a06cca5f5702db6d4f957
czerkies <roman.czerkies@gmail.com>
2015-12-13 18:23:55 +0100

    Feat(gestions-produits): Factorisation code.
    Un seul formulaire pour gérer les produits.
    
    Issue #13

--- vues/produit/gestion_produits.php
+++ vues/produit/gestion_produits.php
@@ -6,29 +6,32 @@
     <li><a href="/lokisalle/www/routeur.php?controleurs=produitAdmin&action=ajouterProduits">Ajouter un produit</a></li>
     <li><a href="/lokisalle/www/routeur.php?controleurs=produitAdmin&action=afficherProduits">Affichage des produits</a></li>
   </ul>
-    <?php if($ajouter){ ?>
-      <div class="gestion_produit_ajouter">
+    <?php if($ajouter || $idProduitModif){ ?>
+      <div class="gestion_produit_modifier">
         <form class="" action="" method="post">
           <select name="id_salle">
             <?php foreach ($affichageSalles as $donneesSalles) { ?>
-              <option value="<?= $donneesSalles['id_salle']; ?>" <?php if(isset($_POST['id_salle']) && $_POST['id_salle'] == $donneesSalles['id_salle']) {echo 'selected';} ?>><?= $donneesSalles['titre'].' - '.$donneesSalles['pays'];?></option>
+              <option value="<?= $donneesSalles['id_salle']; ?>" <?php if(isset($_POST['id_salle']) && $_POST['id_salle'] == $donneesSalles['id_salle']) {echo 'selected';}
+              elseif(isset($idProduitModif['id_salle']) && $idProduitModif['id_salle'] == $donneesSalles['id_salle']) {echo 'selected';} ?>><?= $donneesSalles['titre'].' - '.$donneesSalles['pays'];?></option>
             <?php } ?>
           </select>
           <label for="date_arrivee">Date d'arrivée :</label>
-          <?= $date->champs_date('date_arrivee', 'date_arrivee', NULL); ?>
+          <?= $date->champs_date('date_arrivee', 'date_arrivee', $datebdd = (isset($idProduitModif)) ? $idProduitModif['date_arrivee'] : NULL); ?>
           <label for="date_arrivee">Date d'arrivée :</label>
-          <?= $date->champs_date('date_depart', 'date_depart', NULL); ?>
+          <?= $date->champs_date('date_depart', 'date_depart', $datebdd = (isset($idProduitModif)) ? $idProduitModif['date_depart'] : NULL); ?>
           <label for="prix">Prix :</label>
-          <input type="text" name="prix" id="prix" value="<?php if(isset($_POST['prix'])) {echo $_POST['prix'];} ?>" placeholder="Prix" required> €
+          <input type="text" name="prix" id="prix" value="<?php if(isset($_POST['prix'])) {echo $_POST['prix'];} elseif(isset($idProduitModif['prix'])) {echo $idProduitModif['prix'];} ?>" placeholder="Prix" required> €
           <label for="code_promo">Attribution remise parmi les codes promots existant : </label>
           <select name="id_promo">
             <option disabled>Choisissez votre code promo</option>
             <option value="NULL">Aucun code promo</option>
             <?php foreach ($affichagePromo as $value) { ?>
-              <option value="<?= $value['id_promo']; ?>" <?php if(isset($_POST['id_promo']) && $_POST['id_promo'] == $value['id_promo']) {echo 'selected';} ?>>Code : <?= $value['code_promo'].' | Réduction : '.$value['reduction'];?> €</option>
+              <option value="<?= $value['id_promo']; ?>" <?php if(isset($_POST['id_promo']) && $_POST['id_promo'] == $value['id_promo']) {echo 'selected';}
+              elseif(isset($idProduitModif['id_promo']) && $idProduitModif['id_promo'] == $value['id_promo']) {echo 'selected';} ?>>Code : <?= $value['code_promo'].' | Réduction : '.$value['reduction'];?> €</option>
             <?php } ?>
           </select>
-          <input type="submit" value="Créer le produit">
+          <input type="hidden" name="id_produit" value="<?= $idProduitModif['id_produit']; ?>">
+          <input type="submit" value="Enregistrer">
         </form>
       </div>
     <?php } if($affichageProduitsAdmin) { ?>
@@ -57,61 +60,27 @@
             </tr>
           </thead>
           <tbody>
-            <?php
-            foreach ($affichageProduitsAdmin as $value) {
-            ?>
-            <tr>
-              <td><?= $value['id_produit']; ?></td>
-              <td><?= $value['date_arrivee']; ?></td>
-              <td><?= $value['date_depart']; ?></td>
-              <td><?= $value['id_salle']; ?></td>
-              <?php if($value['code_promo'] == NULL){ ?>
-              <td>Pas de promotion</td>
-              <?php } else { ?>
-              <td><?= $value['code_promo']; ?></td>
-              <?php } ?>
-              <td><?= $value['prix']; ?> €</td>
-              <td><?= $value['etat']; ?></td>
-              <td><a href="/lokisalle/www/routeur.php?controleurs=produitAdmin&action=afficherProduits&modif=<?= $value['id_produit']; ?>">Modifier</a></td>
-              <td><a href="/lokisalle/www/routeur.php?controleurs=produitAdmin&action=afficherProduits&supp=<?= $value['id_produit']; ?>">Supprimer</a></td>
-            </tr>
-            <?php
-            }
-            ?>
+            <?php foreach ($affichageProduitsAdmin as $value) { ?>
+              <tr>
+                <td><?= $value['id_produit']; ?></td>
+                <td><?= $value['date_arrivee']; ?></td>
+                <td><?= $value['date_depart']; ?></td>
+                <td><?= $value['id_salle']; ?></td>
+                <?php if($value['code_promo'] == NULL){ ?>
+                <td>Pas de promotion</td>
+                <?php } else { ?>
+                <td><?= $value['code_promo']; ?></td>
+                <?php } ?>
+                <td><?= $value['prix']; ?> €</td>
+                <td><?= $value['etat']; ?></td>
+                <td><a href="/lokisalle/www/routeur.php?controleurs=produitAdmin&action=afficherProduits&modif=<?= $value['id_produit']; ?>">Modifier</a></td>
+                <td><a href="/lokisalle/www/routeur.php?controleurs=produitAdmin&action=afficherProduits&supp=<?= $value['id_produit']; ?>">Supprimer</a></td>
+              </tr>
+            <?php } ?>
           </tbody>
         </table>
       </div>
-      <?php if($idProduitModif){ ?>
-        <div class="gestion_produit_modifier">
-          <form class="" action="" method="post">
-            <select name="id_salle">
-              <?php foreach ($affichageSalles as $donneesSalles) { ?>
-                <option value="<?= $donneesSalles['id_salle']; ?>" <?php if(isset($_POST['id_salle']) && $_POST['id_salle'] == $donneesSalles['id_salle']) {echo 'selected';}
-                elseif(isset($idProduitModif['id_salle']) && $idProduitModif['id_salle'] == $donneesSalles['id_salle']) {echo 'selected';} ?>><?= $donneesSalles['titre'].' - '.$donneesSalles['pays'];?></option>
-              <?php } ?>
-            </select>
-            <label for="date_arrivee">Date d'arrivée :</label>
-            <?php var_dump($idProduitModif['date_arrivee']); // A SUPPRIMER EN PROD // ?>
-            <?= $date->champs_date('date_arrivee', 'date_arrivee', $idProduitModif['date_arrivee']); ?>
-            <label for="date_arrivee">Date d'arrivée :</label>
-            <?= $date->champs_date('date_depart', 'date_depart', $idProduitModif['date_depart']); ?>
-            <label for="prix">Prix :</label>
-            <input type="text" name="prix" id="prix" value="<?php if(isset($_POST['prix'])) {echo $_POST['prix'];} elseif(isset($idProduitModif['prix'])) {echo $idProduitModif['prix'];} ?>" placeholder="Prix" required> €
-            <label for="code_promo">Attribution remise parmi les codes promots existant : </label>
-            <select name="id_promo">
-              <option disabled>Choisissez votre code promo</option>
-              <option value="NULL">Aucun code promo</option>
-              <?php foreach ($affichagePromo as $value) { ?>
-                <option value="<?= $value['id_promo']; ?>" <?php if(isset($_POST['id_promo']) && $_POST['id_promo'] == $value['id_promo']) {echo 'selected';}
-                elseif(isset($idProduitModif['id_promo']) && $idProduitModif['id_promo'] == $value['id_promo']) {echo 'selected';} ?>>Code : <?= $value['code_promo'].' | Réduction : '.$value['reduction'];?> €</option>
-              <?php } ?>
-            </select>
-            <input type="hidden" name="id_produit" value="<?= $idProduitModif['id_produit']; ?>">
-            <input type="submit" value="Modifier le produit">
-          </form>
-        </div>
-    <?php }
-    } ?>
+  <?php } ?>
 </div>
 <?php } else { ?>
   <p>Vous n'avez pas accès à cette page.</p>
