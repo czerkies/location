475dac2c788a2b97bf122ec07a9eb3eef1730df1
roman <roman.czerkies@gmail.com>
2015-12-26 19:41:00 +0100

    Feat(template): CSS menu header et footer.
    Mise en place du CSS (responsive et desk)
    
    Issue #22

--- vues/menu.inc.php
+++ vues/menu.inc.php
@@ -1,12 +1,10 @@
-<div class="menu_admin">
-  <ul>
-    <li><a href="routeur.php?controleurs=salles&action=gestionSalles">Gestion des salles</a></li>
-    <li><a href="routeur.php?controleurs=produitAdmin&action=afficherProduits">Gestion des produits</a></li>
-    <li><a href="routeur.php?controleurs=membreAdmin&action=gestionMembres">Gestion des membres</li>
-    <li><a href="routeur.php?controleurs=commande&action=gestionCommandes">Gestion des commandes</a></li>
-    <li><a href="routeur.php?controleurs=avis&action=gestionAvis">Gestion des avis</a></li>
-    <li><a href="routeur.php?controleurs=promotion&action=afficherPromotion">Gestion codes promo</a></li>
-    <li><a href="routeur.php?controleurs=statistiques&action=affichageStatistiques">Statistiques</li>
-    <li><a href="routeur.php?controleurs=newsletter&action=envoiNews">Envoyer la newsletter</a></li>
-  </ul>
-</div>
+<ul class="menu_userAdmin">
+  <li><a href="routeur.php?controleurs=salles&action=gestionSalles">Gestion des salles</a></li>
+  <li><a href="routeur.php?controleurs=produitAdmin&action=afficherProduits">Gestion des produits</a></li>
+  <li><a href="routeur.php?controleurs=membreAdmin&action=gestionMembres">Gestion des membres</a></li>
+  <li><a href="routeur.php?controleurs=commande&action=gestionCommandes">Gestion des commandes</a></li>
+  <li><a href="routeur.php?controleurs=avis&action=gestionAvis">Gestion des avis</a></li>
+  <li><a href="routeur.php?controleurs=promotion&action=afficherPromotion">Gestion codes promo</a></li>
+  <li><a href="routeur.php?controleurs=statistiques&action=affichageStatistiques">Statistiques</a></li>
+  <li><a href="routeur.php?controleurs=newsletter&action=envoiNews">Envoyer la newsletter</a></li>
+</ul>
--- vues/template.php
+++ vues/template.php
@@ -2,16 +2,16 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title><?= $title; ?> | Lokisalle</title>
+    <title><?= $title['name']; ?> | Lokisalle</title>
     <meta name="description" content="Lokisalle | Réservez vos salle avec un service clef en main.">
     <link rel="stylesheet" href="<?= RACINE_SITE; ?>css/style.css" charset="utf-8">
   </head>
   <body>
     <div id="nav">
-      <ul>
-        <li><a href="<?= RACINE_SITE; ?>accueil/">Accueil</a></li>
-        <li><a href="<?= RACINE_SITE; ?>nos-salles/">Réservations</a></li>
-        <li><a href="<?= RACINE_SITE; ?>recherche/">Recherche</a></li>
+      <ul class="menu_user">
+        <li><a href="<?= RACINE_SITE; ?>accueil/" <?php if($title['menu'] === 1) echo 'class="active" '; ?>>Accueil</a></li>
+        <li><a href="<?= RACINE_SITE; ?>nos-salles/" <?php if($title['menu'] === 2) echo 'class="active" '; ?>>Réservations</a></li>
+        <li><a href="<?= RACINE_SITE; ?>recherche/" <?php if($title['menu'] === 3) echo 'class="active" '; ?>>Recherche</a></li>
         <?php if(!$userConnect || !$userConnectAdmin){ ?>
         <li><a href="<?= RACINE_SITE; ?>connexion/">Connexion</a></li>
         <?php } if($userConnect || $userConnectAdmin){ ?>
@@ -28,10 +28,10 @@
       <?= $buffer; ?>
     </div>
     <footer>
-      <ul>
-        <li>Mentions légales</li>
-        <li>C.G.V</li>
-        <li>Plan du site</li>
+      <ul class="menu_footer">
+        <li><a href="#">Mentions légales</a></li>
+        <li><a href="#">C.G.V</a></li>
+        <li><a href="#">Plan du site</a></li>
         <li><a href="#" onclick="javascript:window.print()">Imprimer la page</a></li>
         <li><a href="<?= RACINE_SITE; ?>newsletter/">S'inscrire à la newsletter</a></li>
         <li><a href="<?= RACINE_SITE; ?>contact/">Contact</a></li>
