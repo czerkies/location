b6c044511f4273fcca256639495024fb7e5d8830
roman <roman.czerkies@gmail.com>
2016-01-03 18:19:01 +0100

    Feat(statistiques): CSS Admin
    CSS sur la page statistiques.
    
    Issue #31

--- vues/statistiques/statistiques.php
+++ vues/statistiques/statistiques.php
@@ -1,96 +1,92 @@
 <?php if($userConnectAdmin){ $i=1; ?>
 <h2>Statisques</h2>
-<div class="menu_page_active">
-  <ul class="sous-menu-admin">
-    <li><a class="bouton-a <?php if($title['menu_admin'] === 700) echo 'active'; ?>" href="<?= RACINE_SITE; ?>admin/statistiques/salles-mieux-notes/">Top 5 des salles les mieux notés</a></li>
-    <li><a class="bouton-a <?php if($title['menu_admin'] === 701) echo 'active'; ?>" href="<?= RACINE_SITE; ?>admin/statistiques/salles-plus-vendues/">Top 5 des salles les plus vendues</a></li>
-    <li><a class="bouton-a <?php if($title['menu_admin'] === 702) echo 'active'; ?>" href="<?= RACINE_SITE; ?>admin/statistiques/membre-quantite/">Top 5 des membres qui achète le plus</a></li>
-    <li><a class="bouton-a <?php if($title['menu_admin'] === 703) echo 'active'; ?>" href="<?= RACINE_SITE; ?>admin/statistiques/membre-prix/">Top 5 des membres qui achète le plus cher</a></li>
-  </ul>
-</div>
-<div>
+<ul class="sous-menu-admin">
+  <li><a class="bouton-a <?php if($title['menu_admin'] === 700) echo 'active'; ?>" href="<?= RACINE_SITE; ?>admin/statistiques/salles-mieux-notes/#statistiques">Top 5 des salles les mieux notés</a></li>
+  <li><a class="bouton-a <?php if($title['menu_admin'] === 701) echo 'active'; ?>" href="<?= RACINE_SITE; ?>admin/statistiques/salles-plus-vendues/#statistiques">Top 5 des salles les plus vendues</a></li>
+  <li><a class="bouton-a <?php if($title['menu_admin'] === 702) echo 'active'; ?>" href="<?= RACINE_SITE; ?>admin/statistiques/membre-quantite/#statistiques">Top 5 des membres qui achète le plus</a></li>
+  <li><a class="bouton-a <?php if($title['menu_admin'] === 703) echo 'active'; ?>" href="<?= RACINE_SITE; ?>admin/statistiques/membre-prix/#statistiques">Top 5 des membres qui achète le plus cher</a></li>
+</ul>
+<div id="statistiques">
   <?php if($cinqNotes){ ?>
   <h2>Top 5 des salles les mieux notés</h2>
-  <table>
-    <theah>
-      <th></th>
-      <th>ID Salle</th>
-      <th>Titre</th>
-      <th>Note</th>
-    </thead>
-    <tbody>
+  <div class="tableau large">
+    <div class="head">
+      <p class="title wp10"></p>
+      <p class="title wp30">ID Salle</p>
+      <p class="title wp30">Titre</p>
+      <p class="title wp30">Note</p>
+    </div>
+    <ul class="body">
       <?php foreach($cinqNotes as $note) { ?>
-        <tr>
-          <td><?= $i; $i++; ?></td>
-          <td><?= $note['id_salle']; ?></td>
-          <td><?= $note['titre']; ?></td>
-          <td><?= round($note['note'], 2); ?></td>
-        </tr>
+      <li>
+        <p class="cel wp10"><b><?= $i; $i++; ?></b></p>
+        <p class="cel wp30"><?= $note['id_salle']; ?></p>
+        <p class="cel wp30"><?= $note['titre']; ?></p>
+        <p class="cel wp30"><?= round($note['note'], 2); ?></p>
+      </li>
       <?php } ?>
-    </tbody>
-  </table>
+    </ul>
+  </div>
   <?php } if($cinqVendues){ ?>
   <h2>Top 5 des salles les plus vendues</h2>
-  <table>
-    <thead>
-      <th></th>
-      <th>Titre de la salle</th>
-      <th>ID Salle</th>
-      <th>Nombre commandé</th>
-    </thead>
-    <tbody>
+  <div class="tableau large">
+    <div class="head">
+      <p class="title wp10"></p>
+      <p class="title wp30">Titre de la salle</p>
+      <p class="title wp30">ID Salle</p>
+      <p class="title wp30">Nombre commandé</p>
+    </div>
+    <ul class="body">
       <?php foreach ($cinqVendues as $vendues) { ?>
-        <tr>
-          <td><?= $i; $i++; ?></td>
-          <td><?= $vendues['titre']; ?></td>
-          <td><?= $vendues['id_salle']; ?></td>
-          <td><?= $vendues['nbcommande']; ?></td>
-        </tr>
+      <li>
+        <p class="cel wp10"><b><?= $i; $i++; ?></b></p>
+        <p class="cel wp30"><?= $vendues['id_salle']; ?></p>
+        <p class="cel wp30"><?= $vendues['titre']; ?></p>
+        <p class="cel wp30"><?= $vendues['nbcommande']; ?></p>
+      </li>
       <?php } ?>
-    </tbody>
-  </table>
+    </ul>
+  </div>
   <?php } if($cinqMembresQuantite){ ?>
   <h2>Top 5 des membres qui achète le plus</h2>
-  <table>
-    <thead>
-      <th></th>
-      <th>Prénom</th>
-      <th>Nom</th>
-      <th>Nombre de produits achetés</th>
-    </thead>
-    <tbody>
+  <div class="tableau large">
+    <div class="head">
+      <p class="title wp10"></p>
+      <p class="title wp30">Prénom</p>
+      <p class="title wp30">Nom</p>
+      <p class="title wp30">Nombre de produits achetés</p>
+    </div>
+    <ul class="body">
       <?php foreach ($cinqMembresQuantite as $quantite) { ?>
-        <tr>
-          <td><?= $i; $i++; ?></td>
-          <td><?= $quantite['prenom']; ?></td>
-          <td><?= $quantite['nom']; ?></td>
-          <td><?= $quantite['nbproduit']; ?></td>
-        </tr>
+      <li>
+        <p class="cel wp10"><b><?= $i; $i++; ?></b></p>
+        <p class="cel wp30"><?= $quantite['prenom']; ?></p>
+        <p class="cel wp30"><?= strtoupper($quantite['nom']); ?></p>
+        <p class="cel wp30"><?= $quantite['nbproduit']; ?></p>
+      </li>
       <?php } ?>
-    </tbody>
-  </table>
+    </ul>
+  </div>
   <?php } if($cinqMembresPrix){ ?>
   <h2>Top 5 des membres dépensant le plus</h2>
-  <table>
-    <thead>
-      <th></th>
-      <th>Prénom</th>
-      <th>Nom</th>
-      <th>Total</th>
-    </thead>
-    <tbody>
+  <div class="tableau large">
+    <div class="head">
+      <p class="title wp10"></p>
+      <p class="title wp30">Prénom</p>
+      <p class="title wp30">Nom</p>
+      <p class="title wp30">Total</p>
+    </div>
+    <ul class="body">
       <?php foreach ($cinqMembresPrix as $total) { ?>
-        <tr>
-          <td><?= $i; $i++; ?></td>
-          <td><?= $total['prenom']; ?></td>
-          <td><?= $total['nom']; ?></td>
-          <td><?= round($total['total'], 2); ?> €</td>
-        </tr>
+      <li>
+        <p class="cel wp10"><b><?= $i; $i++; ?></b></p>
+        <p class="cel wp30"><?= $total['prenom']; ?></p>
+        <p class="cel wp30"><?= strtoupper($total['nom']); ?></p>
+        <p class="cel wp30"><?= round($total['total'], 2); ?> €</p>
+      </li>
       <?php } ?>
-    </tbody>
-  </table>
+    </ul>
+  </div>
   <?php } ?>
 </div>
-<?php } else { ?>
-<p>Vous n'avez pas accès à cette page</p>
 <?php } ?>
