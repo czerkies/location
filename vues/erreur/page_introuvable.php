074eedbc712135dc3202b0c093314195c431bbc1
czerkies <roman.czerkies@gmail.com>
2016-01-05 09:27:12 +0100

    Fix(page-introuvable): Debug 1.0.1
    Lien Accueil sur la page introuvable incorrect.
    
    Issue #36

--- vues/erreur/page_introuvable.php
+++ vues/erreur/page_introuvable.php
@@ -2,6 +2,6 @@
   <h2>Page introuvable</h2>
   <div class="form-group erreur large">
     <label>Désolé, votre page est introuvable.</label>
-    <p><a href="<?= RACINE_SITE; ?>accueil/">Lokisalle vous permet de louer des salles</a></p>
+    <p><a href="<?= RACINE_SITE; ?>">Lokisalle vous permet de louer des salles</a></p>
   </div>
 </div>
