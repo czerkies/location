5f97837a9aeae51493a40c167ee0ca9401b86b2f
czerkies <roman.czerkies@gmail.com>
2016-01-06 09:43:18 +0100

    Feat(accueil): Mise en page produit
    Mise en page des fiches produits, icone localisation.
    Mise en place du CSS pour le slider.
    
    Issue #40 #41

--- vues/produit/accueil.php
+++ vues/produit/accueil.php
@@ -1,5 +1,11 @@
 <div class="infos_1">
   <h2>À propos de <span>Loki</span>salle</h2>
+  <div class="slide">
+    <img class="img1" src="#" alt="Lokisalle 1">
+    <img class="img2" src="#" alt="Lokisalle 2">
+    <img class="img3" src="#" alt="Lokisalle 3">
+    <img class="img4" src="#" alt="Lokisalle 4">
+  </div>
   <p>Lokisalle est un service de location de salle, clef en main. Les meilleurs salle de réunions vous sont proposées pour un prix imbattable. Sont service de location rapide et efficace vous permettra de réserver votre salle en toutes simplicités.</p>
 </div>
 <div id="last3offres">
@@ -9,11 +15,11 @@
       <a href="<?= RACINE_SITE; ?>nos-salles/reservation-en-details/<?= $value['id_produit']; ?>">
         <h3><?= $value['titre']; ?></h3>
         <p class="image_overflow"><img src="<?= RACINE_SITE.$value['photo']; ?>" alt="<?= $value['titre']; ?>"></p>
-        <p class="adresse"><?= $value['ville']; ?>, <?= $value['pays']; ?></p>
-        <p class="date_arrivee">Date d'arrivée : <?= $value['date_arrivee']; ?></p>
-        <p class="date_depart">Date de départ : <?= $value['date_depart']; ?></p>
-        <p class="capacite">Capacité : <?= $value['capacite']; ?></p>
-        <p class="prix">Prix : <?= $value['prix']; ?> € HT</p>
+        <p class="adresse"><img class="pict" src="<?= RACINE_SITE; ?>pict/localisation.png" alt="Localisation"><?= $value['ville']; ?>, <?= $value['pays']; ?></p>
+        <p class="date_arrivee"><span>Disponible du</span> <?= $value['date_arrivee']; ?></p>
+        <p class="date_depart"><span>au</span> <?= $value['date_depart']; ?></p>
+        <p class="capacite"><span>Capacité :</span> <?= $value['capacite']; ?> places.</p>
+        <p class="prix"><span>Prix :</span> <?= $value['prix']; ?> € HT</p>
       </a>
       <p class="panier">
         <?php if($userConnect){ ?>
