4b46ed664c3335ac9d60a7c86cf2eebf64052a57
roman <roman.czerkies@gmail.com>
2016-01-08 21:37:17 +0100

    Feat(plan-du-site): design plan du site.
    Design plan du site.
    
    Issue #43

--- vues/footer/plan-du-site.php
+++ vues/footer/plan-du-site.php
@@ -1,32 +1,39 @@
 <div id="plan-du-site">
   <h2>Plan du site</h2>
-  <div class="tableau">
-    <div class="head">
-      <p class="title wp100">Visiteur</p>
+  <div class="plan-du-site__visiteur">
+    <div class="tableau">
+      <div class="head">
+        <p class="title wp100">Visiteur</p>
+      </div>
+      <ul class="body">
+        <li><a href="<?= RACINE_SITE; ?>"><p class="cel wp100">Accueil</p></a></li>
+        <li><a href="<?= RACINE_SITE; ?>nos-salles/"><p class="cel wp100">Réservations</p></a></li>
+        <li><a href="<?= RACINE_SITE; ?>recherche/"><p class="cel wp100">Recherche</p></a></li>
+        <?php if(!($userConnect || $userConnectAdmin)){ ?>
+        <li><a href="<?= RACINE_SITE; ?>connexion/"><p class="cel wp100">Connexion</p></a></li>
+        <li><a href="<?= RACINE_SITE; ?>inscription/"><p class="cel wp100">Créer un compte</p></a></li>
+        <?php } ?>
+        <li><a href="<?= RACINE_SITE; ?>mentions-legales/"><p class="cel wp100">Mentions Légales</p></a></li>
+        <li><a href="<?= RACINE_SITE; ?>conditions-generales-de-ventes/"><p class="cel wp100">Conditions générales de ventes</p></a></li>
+        <li><a href="<?= RACINE_SITE; ?>newsletter/"><p class="cel wp100">S'inscrire à la Newsletter</p></a></li>
+        <li><a href="<?= RACINE_SITE; ?>contact/"><p class="cel wp100">Nous contacter</p></a></li>
+      </ul>
     </div>
-    <ul class="body">
-      <li><a href="<?= RACINE_SITE; ?>"><p class="cel wp100">Accueil</p></a></li>
-      <li><p class="cel wp100"><a href="<?= RACINE_SITE; ?>nos-salles/">Réservations</a></p></li>
-      <li><p class="cel wp100"><a href="<?= RACINE_SITE; ?>recherche/">Recherche</a></p></li>
-      <?php if(!($userConnect || $userConnectAdmin)){ ?>
-      <li><p class="cel wp100"><a href="<?= RACINE_SITE; ?>connexion/">Connexion</a></p></li>
-      <li><p><a href="<?= RACINE_SITE; ?>inscription/">Créer un compte</a></p></li>
-      <?php } ?>
-      <li><p class="cel wp100"><a href="<?= RACINE_SITE; ?>mentions-legales/">Mentions Légales</a></p></li>
-      <li><p class="cel wp100"><a href="<?= RACINE_SITE; ?>conditions-generales-de-ventes/">Conditions générales de ventes</a></p></li>
-      <li><p class="cel wp100"><a href="<?= RACINE_SITE; ?>newsletter/">S'inscrire à la Newsletter</a></p></li>
-      <li><p class="cel wp100"><a href="<?= RACINE_SITE; ?>contact/">Nous contacter</a></p></li>
-    </ul>
   </div>
   <?php if($userConnect || $userConnectAdmin){ ?>
-  <div class="form-group">
-    <label>Membres</label>
-    <ul class="body">
-      <li><p class="cel"><a href="<?= RACINE_SITE; ?>mon-profil/">Votre profil</a></p><li>
-      <p><a href="<?= RACINE_SITE; ?>mon-profil/modification/">Modifier votre profil</a></p>
-      <p><a href="<?= RACINE_SITE; ?>panier/">Votre panier</a></p>
-      <p><a onclick="confirmationDeco()" href="#">Se déconnecter</a></p>
-    </ul>
+  <div class="plan-du-site__admin">
+    <div class="tableau">
+      <div class="head">
+        <p class="title wp100">Membre</p>
+      </div>
+      <ul class="body">
+        <li><a href="<?= RACINE_SITE; ?>mon-profil/"><p class="cel wp100">Votre profil</p></a></li>
+        <li><a href="<?= RACINE_SITE; ?>mon-profil/modification/"><p class="cel wp100">Modifier votre profil</p></a></li>
+        <li><a href="<?= RACINE_SITE; ?>panier/"><p class="cel wp100">Votre panier</p></a></li>
+        <li><a onclick="confirmationDeco()" href="#"><p class="cel wp100">Se déconnecter</p></a></li>
+      </ul>
+    </div>
   </div>
-  <?php } ?>
+</div>
+<?php } ?>
 </div>
