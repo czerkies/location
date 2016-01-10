bd2f72a834d5e97511bdf16a6965898096802025
roman <roman.czerkies@gmail.com>
2016-01-10 17:18:58 +0100

    Feat(footer): Améliorations 1.1
    CSS mentions légales, cgv.
    Texte CGV.
    
    Issue #45

--- vues/footer/mentions-legales.php
+++ vues/footer/mentions-legales.php
@@ -1,7 +1,7 @@
-<div>
+<div id="page-footer">
   <h2>Mentions Légales</h2>
   <p>Le directeur de publication du site est Monsieur IMAGINAIRE.</p>
-  <p>Le présent est déconmmé "Lokisalle" est un site fictif.</p>
+  <p>Le présent est dénommé "Lokisalle" est un site fictif.</p>
   <p>Il est réalisé dans le cadre d'une formation à l'IFOCOP à la date du 11/01/2016.</p>
   <p>Site réalisé par Roman Czerkies.</p>
   <p>Site hébergé par : OVH.</p>
